48a5090daf5228461d2b392395b4d4019fd5eeb3
Hamdi LAADHARI <killerwolf06@gmail.com>
2014-05-23 22:30:06 +0200

    using only required services

--- application/app/config/services.php
+++ application/app/config/services.php
@@ -1,26 +1,20 @@
 <?php
 
-use Phalcon\DI\FactoryDefault;
+use Phalcon\DI;
 use Phalcon\Mvc\View;
 use Phalcon\Mvc\Url as UrlResolver;
 use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
 use Phalcon\Mvc\Model\Metadata\Memory as MetaDataAdapter;
 use Phalcon\Session\Adapter\Files as SessionAdapter;
+use Phalcon\Events\Manager as EventsManager;
+use Phalcon\Mvc\Dispatcher as MvcDispatcher;
 
-/**
- * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
- */
-$di = new FactoryDefault();
+$di = new DI();
 
-/**
- * The URL component is used to generate all kind of urls in the application
- */
-$di->set('url', function () use ($config) {
-    $url = new UrlResolver();
-    $url->setBaseUri($config->application->baseUri);
+$di->set('dispatcher', function(){
+    return new MvcDispatcher();
 
-    return $url;
 }, true);
 
 /**
@@ -49,35 +43,6 @@ $di->set('view', function () use ($config) {
 
     return $view;
 }, true);
-
-/**
- * Database connection is created based in the parameters defined in the configuration file
- */
-$di->set('db', function () use ($config) {
-    return new DbAdapter(array(
-        'host' => $config->database->host,
-        'username' => $config->database->username,
-        'password' => $config->database->password,
-        'dbname' => $config->database->dbname
-    ));
-});
-
-/**
- * If the configuration specify the use of metadata adapter use it or use memory otherwise
- */
-$di->set('modelsMetadata', function () {
-    return new MetaDataAdapter();
-});
-
-/**
- * Start the session the first time some component request the session service
- */
-$di->set('session', function () {
-    $session = new SessionAdapter();
-    $session->start();
-
-    return $session;
-});
 
 $di->set('router', function(){
     require __DIR__.'/routes.php';
@@ -108,5 +73,4 @@ $di->set('imagine', function(){
         }
         $className = sprintf('Imagine\%s\Imagine',$drv );
     return new $className;
-});
-
+},true);
