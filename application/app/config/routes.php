cbd68d92acc1efa04a95fdd7ed82b7d2fb882a02
Hamdi LAADHARI <killerwolf06@gmail.com>
2014-06-17 21:58:02 +0200

    WIP

--- application/app/config/routes.php
+++ application/app/config/routes.php
@@ -4,6 +4,8 @@ $router = new \Phalcon\Mvc\Router(false);
 
 $router->add("/", "Img::index");
 
+$router->add("/info", "Img::index");
+
 $router
     ->add(
     	"/{transformation}/{origin}/{width:([0-9]+)}x{height:([0-9]+)}/{parameters:([a-z\-]+\/[0-9]+)+}/{title:([a-zA-Z0-9_-]+)}.{ext}", 
